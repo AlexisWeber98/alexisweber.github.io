c99b0dca197f2a32ca96169fcfc1c2273313e618
Alexis Weber <alexis.weber@mailfence.com>
2024-01-11 17:24:22 -0300

    change resolution on cards images

--- app/components/Cards/Card.tsx
+++ app/components/Cards/Card.tsx
@@ -2,29 +2,33 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./../Proyects/Proyects.module.css";
+import { StaticImageData } from "next/image";
 
 interface CardProps {
   name: string;
-  image: string;
+  image: string | StaticImageData;
   url: string;
   disclamer?: string;
 }
 const Cards: React.FC<CardProps> = ({ name, image, url, disclamer }) => {
-
   return (
-    <main >
+    <main>
       <h4>{name}</h4>
-      <Link  href={url}>
-        <Image
-          className={styles.image}
-          width={100}
-          height={100}
-          alt={name}
-          src={image}
-          style={{ margin: 1, }}  
-        />
-        {disclamer? <p>por el momento el back de este proyecto esta desplegado en render, por lo que puede demorar en renderizar la informacion</p>:null}
-      </Link>
+
+      <Image
+        className={styles.image}
+        width={2000}
+        height={2000}
+        alt={name}
+        src={image}
+        style={{ margin: 1 }}
+      />
+      {disclamer ? (
+        <p>
+          por el momento el back de este proyecto esta desplegado en render, por
+          lo que puede demorar en renderizar la informacion
+        </p>
+      ) : null}
     </main>
   );
 };
