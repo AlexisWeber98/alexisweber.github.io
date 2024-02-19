9b5139385d51cf21f79b77c7800807c80dd79f34
Alexis Weber <alexis.weber@mailfence.com>
2024-02-19 00:20:23 -0300

    semantic refactor

--- app/project-detail/CraftBeer/page.tsx
+++ app/project-detail/CraftBeer/page.tsx
@@ -5,7 +5,7 @@ import {
   fecthFrontTecnologies,
 } from "../../../API/data";
 import { ProjectInterface } from "@/app/components/Proyects/Proyects";
-import styles from "./craeftbeer.module.css";
+import styles from "../styles.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -43,26 +43,33 @@ const techMap = techArray.map((tech) => {
 const CraftbeerDetail = () => {
   return (
     <>
+      <h1 className={styles.h1}>{craftBeerData.name}</h1>
       <section className={styles.contInfo}>
         <ProjectDetail project={craftBeerData} />
         <div>
           <p className={styles.description}>
-            CraftBeer es una plataforma de E-commerce enfocada en la
-            comercialización de cervezas artesanales.
+            <strong>CraftBeer</strong> es una plataforma de <em>E-commerce</em>
+            enfocada en la comercialización de cervezas artesanales de calidad y
+            variedad.
           </p>
           <p className={styles.description}>
-            Por mi parte estuve a la cabeza del equipo de Backend, integrado por
-            4 personas desarrollando la API, base de datos relacional con hasta
-            8 tablas, implementacion de pasarela de pago (mercadopago) y en un
-            paso por Frontend la implementación de la autenticación y
-            autorización de terceros vía Google.
+            Yo fui el líder del equipo de <strong>Backend</strong>, integrado
+            por cuatro personas que desarrollaron la <strong>API</strong>, una
+            base de datos relacional con hasta ocho tablas, la implementación de
+            la pasarela de pago (<strong>MercadoPago</strong>) y, en un paso por
+            el
+            <strong>Frontend</strong>, la implementación de la autenticación y
+            autorización de terceros vía <strong>Google</strong>.
           </p>
           <p className={styles.description}>
-            Para organizar el flujo de trabajo se ulizó SCRUM como metodología
-            ágil y Trello para administrar las tareas del grupo. Además me ocupé
-            del despliegue tanto del Servidor como de la Base de datos en la
-            plataforma Fly.io, configurando así un contenedor de Docker, así
-            como el depliegue del Frontend através de Vercel
+            Para organizar el flujo de trabajo se utilizó <strong>SCRUM</strong>
+            como metodología ágil y <strong>Trello</strong> para administrar las
+            tareas del grupo. Además, me ocupé del despliegue tanto del
+            <strong>Servidor</strong> como de la <strong>Base de datos</strong>{" "}
+            en la plataforma <strong>Fly.io</strong>, configurando así un
+            contenedor de
+            <strong>Docker</strong>, así como el despliegue del{" "}
+            <strong>Frontend</strong>a través de <strong>Vercel</strong>.
           </p>
         </div>
       </section>
@@ -75,7 +82,7 @@ const CraftbeerDetail = () => {
           <div>{techMap}</div>
         </div>
         <hr />
-        <div style={{padding:"1rem"}}>
+        <div style={{ padding: "1rem" }}>
           <div className={styles.link}>
             <Link href={craftBeerData.url}>
               Visita el proyecto haciendo click aquí
