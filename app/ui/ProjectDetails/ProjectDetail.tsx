5b2878c77e28fa8fc13ce23cf59dfb07543e713c
Alexis Weber <alexis.weber@mailfence.com>
2024-02-19 01:23:17 -0300

    add ui folder and projectdetails file

---
+++ app/ui/ProjectDetails/ProjectDetail.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { ProjectInterface } from "@/app/components/Proyects/Proyects";
+import Link from "next/link";
+import styles from "./projectdetail.module.css";
+
+interface ProjectDetailProps {
+  project: ProjectInterface;
+}
+
+const ProjectDetail: React.FC<ProjectDetailProps> = ({ project }) => {
+  const { name, image } = project;
+
+  return (
+    <div className={styles.cont}>
+      <div className={styles.contInfo}>
+        <Image
+          className={styles.projectImage}
+          src={image}
+          alt={name}
+          width={300}
+          height={300}
+        />  
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetail;
