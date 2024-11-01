86a8915aecad6812f56189d7c04795551eb21b42
Alexis Weber <Alexis.weber@mailfence.com>
2024-11-01 16:21:06 -0300

    add ackee script

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,14 @@ export default function RootLayout({
         </div>
         <hr />
         <div>{children}</div>
+        <script
+            async
+            src="https://sh1cc4n7-3000.brs.devtunnels.ms/tracker.js"
+            data-ackee-server="https://sh1cc4n7-3000.brs.devtunnels.ms"
+            data-ackee-domain-id="5771e696-9dbe-4e45-82ef-eb7d4a5240c3"
+            data-ackee-opt='{"ignoreLocalhost":true}'
+            data-ackee-detailed="true"
+            ignore-own-visits="false"></script>
       </body>
     </html>
   );
